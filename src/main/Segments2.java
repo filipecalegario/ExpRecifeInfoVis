a4887c4f3a42740c88aeffdf4613519efce529a2
fcac <filipe.calegario@gmail.com>
2016-12-28 18:24:17 -0300

    Video subclip splitter selected by phase title

--- src/main/Segments2.java
+++ src/main/Segments2.java
@@ -29,12 +29,14 @@ public class Segments2 extends PApplet {
 		cores.put("Planning", color(40,130,130));
 		cores.put("Default", color(151,151,151));
 
-		Table table = loadTable("segmentacaoVideo3.csv", "header");
+		Table table = loadTable("ProcessedMergedCSV_v0.0.3.csv", "header");
 		for (int row = 0; row < table.getRowCount(); row++) {
-			String entrevistado = table.getString(row, "entrevistado");
-			String fase = table.getString(row, "fase");
-			String duracao = table.getString(row, "duracao");
-			String session = table.getString(row, "session");
+			String entrevistado = table.getString(row, "Session-Participant");
+			String fase = table.getString(row, "Phase");
+			String duracao = table.getString(row, "Duration");
+			String session = table.getString(row, "Option");
+			String startTime = table.getString(row, "StartTime");
+			String endTime = table.getString(row, "EndTime");
 			Session s = null;
 			if(sessions.containsKey(entrevistado)){
 				s = sessions.get(entrevistado);
@@ -43,7 +45,7 @@ public class Segments2 extends PApplet {
 				sessions.put(entrevistado, s);
 			}
 			if(s != null){
-				s.addPhase(new Phase(fase, duracao));
+				s.addPhase(new Phase(fase, duracao,startTime,endTime));
 			}
 		}
 	}
@@ -66,6 +68,7 @@ public class Segments2 extends PApplet {
 		System.out.println(out);
 		for (String key : set) {
 			Session s = sessions.get(key);
+			int counter = 1;
 			y = (sectionHeight * indexY++) + upperMargin;
 			x = 0;
 			textAlign(LEFT, TOP);
@@ -80,6 +83,11 @@ public class Segments2 extends PApplet {
 				} else {
 					cor = cores.get("Default");
 				}
+//				if(p.getTitle().equals("Bug")){
+//					cor = cores.get(p.getTitle());
+//				} else {
+//					cor = cores.get("Default");
+//				}
 
 				float largura = durationToPixels(p.getDurationInString());
 
@@ -88,10 +96,19 @@ public class Segments2 extends PApplet {
 				rect(x + leftMargin, y, largura, 20);
 				//System.out.println(entrevistado + " " + currentEntrevistado + " " + fase + " " + largura + " " + x + " " + y);
 				stroke(127);
+				strokeWeight(0.1f);
 				line(0, y, width, y);
 				x = x + largura;
 				if(p.getTitle().equals("Testing")){
 					maxTesting = Math.max(p.getDurationInSeconds(), maxTesting);
+				}
+				
+				if (p.getTitle().equals("Testing") && s.getOption().equals("X")) {
+					String substring = s.getParticipant().substring(3, s.getParticipant().length());
+					String inputVideoFile = substring + ".mov";
+					String outputVideoFile = s.getParticipant() + "_"+p.getTitle()+"_" + counter++ + ".mov";
+					String output = String.format("ffmpeg -i %s -c copy -ss %s -to %s %s &&", inputVideoFile, p.getStartTime(),p.getEndTime(), outputVideoFile);
+					System.out.println(output);
 				}
 			}
 			textAlign(LEFT, TOP);
@@ -100,7 +117,7 @@ public class Segments2 extends PApplet {
 			text(DurationFormat.secondsToString(s.calculateTotalDurationInSeconds()), 810 + leftMargin, y );
 			text(DurationFormat.secondsToString(maxTesting), 810 + leftMargin + 50, y);
 			//System.out.println(s.getParticipant() + ",00:" + DurationFormat.secondsToString(maxTesting));
-			s.printTransactionsReport2();
+			//s.printTransactionsReport2();
 		}
 		text("TOTAL", 810 + leftMargin, 2);
 		text("MAX_TEST", 810 + leftMargin + 50, 2);
@@ -117,11 +134,15 @@ public class Segments2 extends PApplet {
 		//exit();
 	}
 
+	private void generateFfmpegSplitCode(Session s, Phase p) {
+		
+	}
+
 	public float durationToPixels(String duration){
 		return (DurationFormat.stringToSeconds(duration)/2.0f);
 	}
 	public void settings() {  size(1200, 790); }
-	//public void settings() {  size(1200, 790, PDF, "export/segments_v0.0.3.pdf"); }
+	//public void settings() {  size(1200, 790, PDF, "export/segments_bugs.pdf"); }
 	static public void main(String[] passedArgs) {
 		String[] appletArgs = new String[] { "main.Segments2" };
 		if (passedArgs != null) {
--- src/model/Phase.java
+++ src/model/Phase.java
@@ -5,12 +5,22 @@ import util.DurationFormat;
 public class Phase {
 	
 	private String title;
+	private String startTime;
+	private String endTime;
 	private int duration;
 	
 	public Phase(String title, String duration) {
 		super();
 		this.title = title;
 		this.duration = DurationFormat.stringToSeconds(duration);
+	}
+
+	public Phase(String title, String duration, String startTime, String endTime) {
+		super();
+		this.title = title;
+		this.duration = DurationFormat.stringToSeconds(duration);
+		this.startTime = startTime;
+		this.endTime = endTime;
 	}
 
 	public String getTitle() {
@@ -31,6 +41,22 @@ public class Phase {
 
 	public void setDuration(int duration) {
 		this.duration = duration;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
 	}
 	
 }
